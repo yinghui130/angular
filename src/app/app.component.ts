b66a2ff90bc05f5a1ff2307928aa29c9e939adb7
yinghui130 <yinghui130@qq.com>
2017-10-28 14:13:33 +0800

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+  id:number;
+  name:string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} 详情!</h2>
+  <div><label>id:</label>{{hero.id}}</div>
+  <div>
+    <label>name:</label>
+    <input [(ngModel)]="hero.name" placeholder="name"/>
+  </div>
+  `,
+ // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '英雄浏览';
+  //hero='风暴';
+  hero:Hero={
+    id:1,
+    name:'风暴'
+  }
 }
